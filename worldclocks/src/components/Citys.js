1c3c85517fba43b7e845f2ae0f7def95f2f7b65e
volkanGoksu <volkan.goksu@outlook.com>
2021-05-15 16:32:38 +0300

    şehirleri ekranda listeleme

--- worldclocks/src/components/Citys.js
+++ worldclocks/src/components/Citys.js
@@ -1,20 +1,33 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import axios from 'axios';
 
 export default function Citys() {
+
+    const [citys,setCitys] = useState([])
+
     useEffect(()=>{
         getCitys();
     },[])
+
     const getCitys = ()=>{
        axios.get('http://worldtimeapi.org/api/timezone')
-       .then(res=>{
-           console.log(res.data);
+       .then(response=>{
+         //  console.log(res.data);
+           setCitys(response.data)
        })
     }
         
     return (
-        <div>
-           
+        <div className="row">
+           {citys.map(city=>(
+               <div className="col l3 m3 s6" city={city}>
+                   <div className = "card blue-grey" > 
+                      <div className = "card-content white-text">
+                          <p>{city}</p>
+                      </div>
+                   </div>
+               </div>
+           ))}
         </div>
     )
 }
