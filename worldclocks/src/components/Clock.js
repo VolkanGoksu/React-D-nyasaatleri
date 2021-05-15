e721a9e34bc81627a2ec2d3189a5b477245092bb
volkanGoksu <volkan.goksu@outlook.com>
2021-05-15 19:04:40 +0300

    npm moment

--- worldclocks/src/components/Clock.js
+++ worldclocks/src/components/Clock.js
@@ -1,9 +1,10 @@
 import React,{useEffect,useState} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
-
+import moment from 'moment'
 export default function Clock({match}) {
    const [clock,setClock] = useState('')
+  
 
     useEffect(()=>{
         //console.log(match.params);
@@ -24,8 +25,8 @@ export default function Clock({match}) {
                   <i className="material-icons">undo</i>
                </Link>
                <div className="card-content">
-                   <h4>{match.params.place}</h4>
-                   <h3>{clock}</h3>
+                   <h4 className='red-text'>{match.params.place}</h4>
+                   <h3>{moment(clock).format('MMMM Do YYYY, h:mm:ss a')}</h3>
                </div>
             </div>
         </div>
